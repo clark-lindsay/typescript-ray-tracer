2ccb48ac35201cd72f2af877225e79d5c9c930de
clark-lindsay <clarklindsay96@gmail.com>
2020-03-30 13:10:51 -0400

    Created a scaling translation to change the size of the components of objects multiplicatively

--- src/transformations.ts
+++ src/transformations.ts
@@ -7,3 +7,11 @@ export function translation(x: number, y: number, z: number): Matrix {
   result[2][3] = z;
   return new Matrix(result);
 }
+
+export function scalingTransformation(x: number, y: number, z: number): Matrix {
+  const result = identityGrid(4);
+  result[0][0] = x;
+  result[1][1] = y;
+  result[2][2] = z;
+  return new Matrix(result);
+}
--- test/transformations.test.ts
+++ test/transformations.test.ts
@@ -1,28 +1,52 @@
-import { translation } from '../src/transformations';
+import { translation as translationTransform, scalingTransformation } from '../src/transformations';
 import { point, vector } from '../src/Tuple';
 
 describe('the Matrix transformation functions', () => {
   describe('the translation function', () => {
     it('returns a 4x4 Matrix that can move a point in space by multiplication', () => {
-      const translationTransform = translation(5, -3, 2);
+      const translation = translationTransform(5, -3, 2);
       const p = point(-3, 4, 5);
 
-      expect(translationTransform.multipliedBy(p).isEqualTo(point(2, 1, 7))).toBeTruthy();
+      expect(translation.multipliedBy(p).isEqualTo(point(2, 1, 7))).toBeTruthy();
     });
 
     it('its inverse creates the exactly opposite translation', () => {
-      const translationTransform = translation(5, -3, 2);
-      const translationInverse = translationTransform.inverse();
+      const translation = translationTransform(5, -3, 2);
+      const translationInverse = translation.inverse();
       const p = point(-3, 4, 5);
 
       expect(translationInverse.multipliedBy(p).isEqualTo(point(-8, 7, 3))).toBeTruthy();
     });
 
     it('will not have any effect when multiplied with a vector', () => {
-      const translationTransform = translation(5, -3, 2);
+      const translation = translationTransform(5, -3, 2);
       const v = vector(-3, 4, 5);
 
-      expect(translationTransform.multipliedBy(v).isEqualTo(v)).toBeTruthy();
+      expect(translation.multipliedBy(v).isEqualTo(v)).toBeTruthy();
+    });
+  });
+
+  describe('the scaling transformation', () => {
+    it('returns a 4x4 Matrix that scales the components of a point or vector ', () => {
+      const transform = scalingTransformation(2, 3, 4);
+      const p = point(-4, 6, 8);
+
+      expect(transform.multipliedBy(p).isEqualTo(point(-8, 18, 32))).toBeTruthy();
+    });
+
+    it('the scaling matrix will affect a vector in the same way that it affects a point', () => {
+      const transform = scalingTransformation(2, 3, 4);
+      const v = vector(-4, 6, 8);
+
+      expect(transform.multipliedBy(v).isEqualTo(vector(-8, 18, 32))).toBeTruthy();
+    });
+
+    it('its inverse creates the exactly opposite translation', () => {
+      const transform = scalingTransformation(2, 3, 4);
+      const scalingInverse = transform.inverse();
+      const v = vector(-4, 5, 8);
+
+      expect(scalingInverse.multipliedBy(v).isEqualTo(vector(-2, 5 / 3, 2))).toBeTruthy();
     });
   });
 });
