19107e2fb70d0e24b35f7160a27ded1d24cef7f2
clark-lindsay <clarklindsay96@gmail.com>
2020-04-06 13:05:48 -0400

    Collapsed the parts of the result color into a single result object within the function to align with my convention and improve readability

--- src/lighting.ts
+++ src/lighting.ts
@@ -8,27 +8,27 @@ export function lighting({ material, light, pointBeingLit, eyePosition, normalVe
   const directionToLightSource = light.position.subtract(pointBeingLit).normalize();
   const directionToEye = eyePosition.subtract(pointBeingLit).normalize();
 
+  const result = { ambient: new Color(0, 0, 0), diffuse: new Color(0, 0, 0), specular: new Color(0, 0, 0) };
+
   const cosineOfAngleBetweenLightAndNormal = directionToLightSource.dotProduct(normalVector);
-  let ambient = effectiveColor.scalarMultiply(material.ambient);
-  let diffuse = new Color(0, 0, 0);
-  let specular = new Color(0, 0, 0);
+  result.ambient = effectiveColor.scalarMultiply(material.ambient);
   if (cosineOfAngleBetweenLightAndNormal < 0) {
     const black = new Color(0, 0, 0);
-    [diffuse, specular] = [black, black];
+    [result.diffuse, result.specular] = [black, black];
   } else {
-    diffuse = effectiveColor.scalarMultiply(material.diffuse).scalarMultiply(cosineOfAngleBetweenLightAndNormal);
+    result.diffuse = effectiveColor.scalarMultiply(material.diffuse).scalarMultiply(cosineOfAngleBetweenLightAndNormal);
     const cosineOfAngleBetweenReflectionAndEye = directionToLightSource
       .negate()
       .reflect(normalVector)
       .dotProduct(directionToEye);
     if (cosineOfAngleBetweenReflectionAndEye <= 0) {
-      specular = new Color(0, 0, 0);
+      result.specular = new Color(0, 0, 0);
     } else {
       const factor = Math.pow(cosineOfAngleBetweenReflectionAndEye, material.shininess);
-      specular = light.intensity.scalarMultiply(material.specular).scalarMultiply(factor);
+      result.specular = light.intensity.scalarMultiply(material.specular).scalarMultiply(factor);
     }
   }
-  return ambient.add(diffuse).add(specular);
+  return result.ambient.add(result.diffuse).add(result.specular);
 }
 
 interface LightingArguments {
